0ea06d7bf1c37c290ff66063a1d4f7258e99b00e
Tomasz Gorka <tomasz@gorka.org.pl>
2017-03-05 20:33:22 +0100

    propts

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,34 +11,91 @@ module.exports = Generator.extend({
         ));
 
         var prompts = [{
-            type: 'confirm',
-            name: 'someAnswer',
-            message: 'Would you like to enable this option?',
-            default: true
+            type: 'input',
+            name: 'name',
+            message: 'What\'s your project name? (whitepaces will be removed)',
+            default: '',
+            store: true
+        }, {
+            type: 'input',
+            name: 'description',
+            message: 'What\'s the project description?',
+            default: '',
+            store: true
+        }, {
+            type: 'input',
+            name: 'version',
+            message: 'What\'s the project version?',
+            default: '0.1.0',
+            store: true
+        }, {
+            type: 'input',
+            name: 'authorName',
+            message: 'What\'s the author name?',
+            default: '',
+            store: true
+        }, {
+            type: 'input',
+            name: 'authorEmail',
+            message: 'What\'s the author email?',
+            default: '',
+            store: true
         }];
 
         return this.prompt(prompts).then(function (props) {
             // To access props later use this.props.someAnswer;
+            for (var prop in props) {
+                props[prop] = props[prop].trim();
+            }
+            props.name = props.name.replace(/\s+/g, '');
+            props.author = '';
+            if (props.authorName) {
+                props.author += props.authorName;
+            }
+            if (props.authorName && props.authorEmail) {
+                props.author += ' ';
+            }
+            if (props.authorEmail) {
+                props.author += '<' + props.authorEmail + '>';
+            }
             this.props = props;
         }.bind(this));
     },
 
     writing: function () {
-        this.fs.copy(
+        this.fs.copyTpl(
+            this.templatePath('**/.*'),
+            this.destinationPath('.'),
+            {
+                globOptions: { dot: true }
+            }
+        );
+        this.fs.copyTpl(
             this.templatePath('**/*'),
             this.destinationPath('.'),
-            { globOptions: { dot: true } }
+            this.defaults().properties
         );
     },
 
+    installingYarn: function() {
+        this.npmInstall(['yarn'], { 'save-dev': true });
+    },
+
     install: function () {
-        this.installDependencies();
+        // copy templates
         this.config.save();
         this.config.defaults(this.defaults());
+        // install dependencies
+        this.installDependencies({
+            npm: false,
+            bower: false,
+            yarn: true
+        });
     },
 
     defaults: function() {
         return {
+            properties: this.props,
             version: "0.0.2"
         }
     }
